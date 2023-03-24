6809f196b8881fb4bb03607553c44ac9cc076cba
Olha Tsyhanenko <olya.tsyganenko@gmail.com>
2023-03-24 13:34:52 +0200

    add section partners

--- js/main.js
+++ js/main.js
@@ -5,10 +5,11 @@ $(document).ready(function(){
     arrows: false,
     initialSlide: 2,
     centerMode: true,
+    slidesToShow: 1,
     centerPadding: '25px',
     responsive: [
       {
-        breakpoint: 480,
+        breakpoint: 479,
         settings: {
           centerPadding: '130px',
         }
@@ -57,6 +58,72 @@ $(document).ready(function(){
           arrows: true,
           slidesToShow: 4,
           centerMode: false,
+        }
+      }
+    ]
+  });
+});
+
+$(document).ready(function(){
+  $('.partners__slider').slick({
+    mobileFirst: true,
+    dots: true,
+    arrows: false,
+    initialSlide: 2,
+    slidesToShow: 2,
+    centerMode: true,
+    centerPadding: '25px',
+    responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          centerPadding: '130px',
+        }
+      },
+      {
+        breakpoint: 550,
+        settings: {
+          centerPadding: '30px',
+          slidesToShow: 2,
+        }
+      },{
+        breakpoint: 620,
+        settings: {
+          centerPadding: '60px',
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          centerPadding: '100px',
+          slidesToShow: 3,
+        }
+      },{
+        breakpoint: 825,
+        settings: {
+          centerPadding: '25px',
+          slidesToShow: 3,
+        }
+      },{
+        breakpoint: 920,
+        settings: {
+          centerPadding: '70px',
+          slidesToShow: 4,
+        }
+      },{
+        breakpoint: 1199,
+        settings: {
+          arrows: true,
+          slidesToShow: 4,
+          centerPadding: '130px',
+        }
+      }, {
+        breakpoint: 1919,
+        settings: {
+          arrows: true,
+          slidesToShow: 4,
+          centerPadding: '300px',
         }
       }
     ]
