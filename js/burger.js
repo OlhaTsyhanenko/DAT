670bbc113d80bb26a0d26b35e8699b17729bf7c2
Olha Tsyhanenko <olya.tsyganenko@gmail.com>
2023-04-06 16:15:45 +0300

    add header

--- js/burger.js
+++ js/burger.js
@@ -1,5 +1,7 @@
 let menuBtn = document.querySelector('.nav__btn');
 let menu = document.querySelector('.nav__inner');
+let background = document.querySelector('.header__nav');
 menuBtn.addEventListener('click', function(){
 	menu.classList.toggle('active');
+	background.classList.toggle('active');
 })
